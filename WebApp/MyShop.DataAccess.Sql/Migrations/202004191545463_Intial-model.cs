5d1ffce61ca7aee1d2027fb67836ce9466c10163
ahmedmagdy <ahmedmagdy492@yahoo.com>
2020-04-19 17:47:55 +0200

    added entity framework

---
+++ WebApp/MyShop.DataAccess.Sql/Migrations/202004191545463_Intial-model.cs
@@ -0,0 +1,47 @@
+namespace MyShop.DataAccess.Sql.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Intialmodel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 15),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Image = c.String(),
+                        Category = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                        Category_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.Category_Id)
+                .Index(t => t.Category_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "Category_Id" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
